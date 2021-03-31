414a91ca50938e2f1a45e17a8e3a17b56e06b24a
zukui <71074389+zukui1984@users.noreply.github.com>
2021-03-31 22:58:09 +0200

    add lib + commerce.js

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
   { id: 1, name: 'Shoes', description: 'Running shoes', price:'$5', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg05b-85alIm13obr0jrRHzx-M0UtAY6Vh0MEVQwFIiEyfhXJUzdoUWYQGKwdDCx9jSIoIZ7He&usqp=CAc'},
@@ -9,8 +10,10 @@ const products = [
 ]
 
 const Products = () => {
+  const classes = useStyles();
   return (
-  <main>
+  <main className={classes.content}>
+  <div className={classes.toolbar}/>
     <Grid container justify="center" spacing={4}>
       {products.map((product) => (
         <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
