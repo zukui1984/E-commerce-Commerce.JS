97bc189631cf096666f0307a6491d962834dff60
zukui <71074389+zukui1984@users.noreply.github.com>
2021-04-02 21:21:45 +0200

    add cart buttons

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Container, Typography, Button, Grid } from "@material-ui/core";
-
+import { Link } from 'react-router-dom';
 import useStyles from "./styles";
 import CartItem from './CartItem/CartItem';
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart}) => {
   const classes = useStyles();
 
   const EmptyCart = () => (
     <Typography variant="subtitle1">
-      You have no item in your shopping cart, please add more item!
+      You have no item in your shopping cart,
+      <Link to="/" className={classes.link}>start adding some</Link>!
     </Typography>
   );
 
@@ -18,7 +19,11 @@ const Cart = ({ cart }) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <CartItem item={item}/>
+            <CartItem 
+            item={item}
+            onUpdateCartQty={handleUpdateCartQty}
+            onRemoveFromCart={handleRemoveFromCart}
+            />
           </Grid>
         ))}
       </Grid>
@@ -33,6 +38,7 @@ const Cart = ({ cart }) => {
             type="button"
             variant="contained"
             color="secondary"
+            onClick={handleEmptyCart}
           >
             Empty Cart
           </Button>
